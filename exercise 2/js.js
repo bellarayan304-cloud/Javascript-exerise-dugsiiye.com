3fb1e4a3bb939967707d178328b7fdf328cf376a
bellarayan304-cloud <bellarayan304@gmail.com>
2026-07-11 00:14:39 +0300

    sumited exercise 2

---
+++ exercise 2/js.js
@@ -0,0 +1,19 @@
+let a = 15;
+let b = "15";
+
+//equal
+console.log(a == b)
+//not equal
+console.log(a != b)
+//Strictly equal
+console.log(a === b)
+//Strictly not equal
+console.log(a !== b)
+// greater than
+console.log(a > b)
+// less than
+console.log(a < b)
+// greater than or equal
+console.log(a >= b)
+//less than or equal
+console.log(a <= b)
