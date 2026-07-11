e15c69d4af87398b5a661871cdabe315bb5fe187
bellarayan304-cloud <bellarayan304@gmail.com>
2026-07-12 01:10:59 +0300

    submit exercise 4

---
+++ exercise 4/funExpress.js
@@ -0,0 +1,16 @@
+function add(a,b){
+console.log(a+b)
+}
+add(20,30)
+add(10,90)
+add(5,15)
+add(90,5)
+add(10,50)
+                            
+
+const great = function(name){
+    console.log('hello ' + name)
+}
+great("layla");
+great("maryam");
+great("apdalla");
