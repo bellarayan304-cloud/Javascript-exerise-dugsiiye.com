78723072ba8901ca39cb1e241ca2f43bededcfd6
bellarayan304-cloud <bellarayan304@gmail.com>
2026-07-15 23:17:52 +0300

    submit exercise 9

---
+++ exercise 9/start.js
@@ -0,0 +1,11 @@
+let car = {
+    make : "japan",
+    model : "las model",
+    year : 2026,
+
+    start: ()=>{
+    console.log("The Car Has Started")
+    }
+}
+console.log(car)
+console.log(car.start())
