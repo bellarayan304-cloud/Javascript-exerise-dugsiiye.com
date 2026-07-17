76e1dd21d2fb03065c717a8a67ddb8f775aadda4
bellarayan304-cloud <bellarayan304@gmail.com>
2026-07-17 22:44:19 +0300

    submit exercise 10

---
+++ exercise 10/arry.js
@@ -0,0 +1,14 @@
+let library=[
+    {
+        title:"Rich Daad Poor Daad",
+         Author:" Robert T. Kiyosaki",
+         Year: 1977,
+
+    },
+    {
+        title: "Richest Man in Babylon",
+        Author:"George S. Clason",
+        Year: 1926,
+    }
+]
+console.log(library[0].Author)
