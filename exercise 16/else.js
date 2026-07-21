a928c5105b8b0923f79a7c61c10b47051d39029c
bellarayan304-cloud <bellarayan304@gmail.com>
2026-07-21 09:57:19 +0300

    submit exercise 16 and 17

---
+++ exercise 16/else.js
@@ -0,0 +1,6 @@
+let score = 50;
+if(score >= 50){
+    console.log("you're passed")
+}else{
+    console.log("you're failed")
+}
---
+++ exercise 17/yamal.js
@@ -0,0 +1,11 @@
+let temperetur = 26 ;
+if(temperetur <= 0){
+    console.log("very cold")
+}
+if(temperetur <= 15 ){
+      console.log("cold")
+}if(temperetur <= 25){
+      console.log("warm")
+}else{
+      console.log("hot")
+}
